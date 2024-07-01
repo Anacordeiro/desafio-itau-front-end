3002ba9011e27da813a1fc38bcd36d3aa8b3894e
Ana Carolina <anacarolina@MacBook-Pro-de-Ana-352.local>
2024-07-01 12:01:43 -0300

    ajustes de componentes editar, cadastrar e excluir

--- src/app/navegacao/home/home.component.ts
+++ src/app/navegacao/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Produto} from 'src/app/models/produtos.model';
+import {ProdutoService} from 'src/app/services/produtos.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  constructor(private produtoService: ProdutoService){}
+  public produtos: Produto[];
+
+
+  ngOnInit(): void{
+    this.retornaProdutos();
+  }
+
+
+  retornaProdutos(){
+
+      this.produtoService.obterProdutos()
+      .subscribe(
+      produtos => {
+          this.produtos = produtos;
+          console.log(produtos)
+        },
+      error => console.log(error)
+      )
+
+      // this.produtoService.
+  }
 
 }
