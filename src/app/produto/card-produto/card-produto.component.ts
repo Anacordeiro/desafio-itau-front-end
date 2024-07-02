57df1c6fb23926fe5fb23727639ba027ef7549a4
Ana Carolina <anacarolina@MacBook-Pro-de-Ana-352.local>
2024-07-02 11:03:13 -0300

    Ajustes de funcionamento tela de editar produto

--- src/app/produto/card-produto/card-produto.component.ts
+++ src/app/produto/card-produto/card-produto.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output,  } from '@angular/core';
-import {Produto} from 'src/app/models/produtos.model';
+import {Produto} from 'src/app/produto/models/produtos.model';
 import {ProdutoService} from 'src/app/services/produtos.service';
 
 @Component({
--- src/app/produto/editar-produto/editar-produto.component.ts
+++ src/app/produto/editar-produto/editar-produto.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {ProdutoService} from 'src/app/services/produtos.service';
+import {Produto} from '../models/produtos.model';
 
 @Component({
   selector: 'app-editar-produto',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./editar-produto.component.css']
 })
 export class EditarProdutoComponent {
+
+  produto: Produto;
+
+  constructor(private route: ActivatedRoute, private produtoService: ProdutoService){
+
+  }
+
+
+  ngOnInit() {
+    this.route.params
+      .subscribe(params => {
+       this.produto = this.produtoService.obterPorId(params['id']);
+    })
+  }
+
+  editarProduto() {
+    // if (this.produtoForm.dirty && this.produtoForm.valid) {
+  }
 
 }
